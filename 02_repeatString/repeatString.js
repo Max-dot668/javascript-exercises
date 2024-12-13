20b982ec6da56fe13910ba5294ea198b2580c90b
Max-dot668 <maxiyouit@gmail.com>
2024-12-13 15:30:32 -0800

    Implement repeatString function and ensure all test pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let word_looped = "";
 
+    if (num < 0) {
+	word = "ERROR";
+	return word;
+    }
+    else {
+
+    for (let i = 0; i < num; i++) {
+	word_looped += word;
+    }
+ 
+	return word_looped;
+    }
 };
 
 // Do not edit below this line
