d52e7d8f26e7d7dfd5dab14a8f40498d21c5b82f
Max-dot668 <maxiyouit@gmail.com>
2024-12-13 19:01:03 -0800

    Implement string reversal function and enable all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let i = word.length - 1;
+    let reverseString = "";
+    while (i >= 0) {
+	reverseString += word.at(i);
+	i--;
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
