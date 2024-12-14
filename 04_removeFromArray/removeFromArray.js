fc8bb0ab954779000aafffe8dedf74755e255b79
Max-dot668 <maxiyouit@gmail.com>
2024-12-14 15:12:25 -0800

    Implement logic for removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, w, x, y, z){
+    return arr.filter(element => element !== w && element !== x && element !== y && element !== z)
+
 };
 
 // Do not edit below this line
