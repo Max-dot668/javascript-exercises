325ca740dd502233e31f57c43fa7dd2d9cb01e47
Max-dot668 <maxiyouit@gmail.com>
2024-12-15 13:52:34 -0800

    Implement logic for temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+    let celsius = (fahr - 32) * (5.0 / 9.0);
+    let roundedCelsius = Number(celsius.toFixed(1));
+    return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    let fahr = celsius * (9.0 / 5.0) + 32;
+    let roundedFahr = Number(fahr.toFixed(1));
+    return roundedFahr;
 };
 
 // Do not edit below this line
