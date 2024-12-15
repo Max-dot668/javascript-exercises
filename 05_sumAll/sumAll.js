310a6435a56ccd57a15ca79c2cd8f4af1a641fcf
Max-dot668 <maxiyouit@gmail.com>
2024-12-14 17:01:07 -0800

    Implement logic for sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(begin, end) {
+    let sum = 0;
+    
+    // check for non-integer parameters
+    if (begin < 0 || end < 0) {
+	sum = "ERROR";
+	return sum;
+    }
+    // check for non-number parameters
+    if (!(Number.isInteger(begin)) || !(Number.isInteger(end))) {
+	sum = "ERROR";
+	return sum;
+    }
+    
+    if (begin > end) {
+	for (let i = begin; i >= end; i--) {
+	    sum += i;
+	}
+    }
+    else { // begin < end
+	for (let i = begin; i <= end; i++) {
+	    sum += i;
+	}
+    }
+    return sum;
 };
 
 // Do not edit below this line
