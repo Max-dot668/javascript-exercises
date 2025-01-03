54c653c1c99f7fb74823748eb8a62d09d7ea7f73
Max-dot668 <maxiyouit@gmail.com>
2025-01-03 14:27:14 +0900

    Implement logic for palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  word = word.split(" ").join('');
+  let reversed = word.split("").reverse().join('');
+  let lowerCaseWord = word.toLowerCase();
+  let lowerCaseReversed = reversed.toLowerCase();
+  
+  let checkWord = lowerCaseWord.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+  let checkReversed = lowerCaseReversed.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
 
+  return checkWord === checkReversed;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
