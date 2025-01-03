d6e98b6b71cc1bf8a59c1cd8b58783d89a605d3a
Max-dot668 <maxiyouit@gmail.com>
2025-01-03 14:36:00 +0900

    Implement logic for findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  // Get current year for people still alive
+  const currentYear = new Date().getFullYear();
+  
+  let ages = arr.map(item => {
+    const death = item.yearOfDeath || currentYear;
+    return death - item.yearOfBirth;  // this return is just for the map  
+  });
+  
+  let oldestAge = 0;
+  let index = 0;
+  
+  for (let i = 0; i < ages.length; i++) {
+      if (oldestAge < ages[i]) {
+          oldestAge = ages[i];
+          index = i;
+      }
+  }
 
+  return arr[index];
 };
 
 // Do not edit below this line
